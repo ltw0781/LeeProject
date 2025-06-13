7d308bc947ff0b73f3b538d493789c5e10f56144
ltw0781 <ltw0781@naver.com>
2025-06-13 16:07:54 +0900

    2025-06-13 게시판 기본 만들기

---
+++ study/src/main/java/com/student/study/dto/Board.java
@@ -0,0 +1,17 @@
+package com.student.study.dto;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class Board {
+    
+    private int no;
+    private String title;
+    private String writer;
+    private String content;
+    private Date createdAt;
+    private Date updatedAt;
+
+}
---
+++ study/src/main/java/com/student/study/service/BoardServiceImpl.java
@@ -0,0 +1,47 @@
+package com.student.study.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.student.study.dto.Board;
+import com.student.study.mapper.BoardMapper;
+
+@Service
+public class BoardServiceImpl implements BoardService{
+
+    @Autowired
+    BoardMapper boardMapoper;
+
+    @Override
+    public List<Board> list() throws Exception {
+        List<Board> boardList = boardMapoper.list();
+        return boardList;
+    }
+
+    @Override
+    public Board select(int no) throws Exception {
+        Board board = boardMapoper.select(no);
+        return board;
+    }
+
+    @Override
+    public int insert(Board board) throws Exception {
+        int result = boardMapoper.insert(board);
+        return result;
+    }
+
+    @Override
+    public int update(Board board) throws Exception {
+        int result = boardMapoper.update(board);
+        return result;
+    }
+
+    @Override
+    public int delete(int no) throws Exception {
+        int result = boardMapoper.delete(no);
+        return result;
+    }
+    
+}
