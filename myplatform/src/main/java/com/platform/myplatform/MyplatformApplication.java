1264e7ac615fe181ba60cb5a469caaadc3280953
ltw0781 <ltw0781@naver.com>
2025-07-02 11:45:36 +0900

    2025-07-02 프로젝트 시작

---
+++ myplatform/src/main/java/com/platform/myplatform/MyplatformApplication.java
@@ -0,0 +1,13 @@
+package com.platform.myplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyplatformApplication.class, args);
+	}
+
+}
---
+++ myplatform/src/test/java/com/platform/myplatform/MyplatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platform.myplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyplatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
